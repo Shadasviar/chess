8e49fcae923291b071e942911aa720167608edaa
Shadasviar <shadasviarus@gmail.com>
2016-11-21 19:31:43 +0100

    moving test

--- sources/test_game.cpp
+++ sources/test_game.cpp
@@ -7,6 +7,18 @@ test_game::test_game()
     game g;
     auto a = g.get_move_cells({0,6});
     auto b = g.get_attack_cells({0,6});
+
+    g.move({0,6},{1,5});
+    a = g.get_move_cells({0,6});
+    b = g.get_attack_cells({0,6});
+    a = g.get_move_cells({1,5});
+    b = g.get_attack_cells({1,5});
+
+    g.move({1,5}, {1,4});
+    a = g.get_move_cells({1,4});
+    b = g.get_move_cells({1,5});
+
+
     a = g.get_move_cells({0,5});
     b = g.get_attack_cells({0,5});
     a = g.get_move_cells({3,5});
